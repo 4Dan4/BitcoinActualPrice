477b38493e3f14b3d022fd416eecba3afe243552
dysya <houmi250204@gmail.com>
2022-05-07 00:59:23 +0300

    Initial Commit

---
+++ BitcoinActualPrice/BitcoinActualPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinActualPriceApp.swift
+//  BitcoinActualPrice
+//
+//  Created by dan4 on 07.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinActualPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
